b4403400f4660e57374da1a31913cfebab465d31
Mohamed Kamel <muhamed.kamel.elsayed@gmail.com>
2025-01-03 23:45:21 +0200

    chore: install linters & apply linting rules

--- lib/index.ts
+++ lib/index.ts
@@ -1,32 +1,43 @@
-
-import mongoose from "mongoose";
-import * as pluralize from "pluralize";
-import { NamingConvention } from "./naming-convention.enum";
-import { toCamelCase, toKebabCase, toPascalCase, toSnakeCase } from "./string.utils";
+import mongoose from 'mongoose';
+import * as pluralize from 'pluralize';
+import { NamingConvention } from './naming-convention.enum';
+import {
+  toCamelCase,
+  toKebabCase,
+  toPascalCase,
+  toSnakeCase,
+} from './string.utils';
 
 type CaseConverter = (input: string) => string;
 
-function getCaseConverter(namingConvention?: NamingConvention):CaseConverter {
-    switch (namingConvention) {
-        case NamingConvention.CamelCase:
-            return toCamelCase
-        case NamingConvention.SnakeCase:
-            return toSnakeCase
-        case NamingConvention.KebabCase:
-            return toKebabCase
-        case NamingConvention.PascalCase:
-            return toPascalCase
-        default:
-            return toCamelCase
-    }
+function getCaseConverter(namingConvention?: NamingConvention): CaseConverter {
+  switch (namingConvention) {
+    case NamingConvention.SnakeCase:
+      return toSnakeCase;
+
+    case NamingConvention.KebabCase:
+      return toKebabCase;
+
+    case NamingConvention.PascalCase:
+      return toPascalCase;
+
+    case NamingConvention.CamelCase:
+
+    // fall through
+    default:
+      return toCamelCase;
+  }
 }
 
 function convertCollectionName(collectionName: string): string {
-    const pluralizedCollectionName = pluralize(collectionName);
+  const pluralizedCollectionName = pluralize(collectionName);
 
-    return getCaseConverter(this?.namingConvention)(pluralizedCollectionName);
+  return getCaseConverter(this?.namingConvention)(pluralizedCollectionName);
 }
 
-export function MongooseCaseConverterPlugin(schema: mongoose.Schema, options?: { namingConvention?: NamingConvention }) {
-    mongoose.pluralize(convertCollectionName.bind(options));
+export function MongooseCaseConverterPlugin(
+  schema: mongoose.Schema,
+  options?: { namingConvention?: NamingConvention },
+): void {
+  mongoose.pluralize(convertCollectionName.bind(options));
 }
--- lib/naming-convention.enum.ts
+++ lib/naming-convention.enum.ts
@@ -1,6 +1,6 @@
 export enum NamingConvention {
-	CamelCase = 'CamelCase',
-	SnakeCase = 'SnakeCase',
-	KebabCase = 'KebabCase',
-	PascalCase = 'PascalCase'
+  CamelCase = 'CamelCase',
+  SnakeCase = 'SnakeCase',
+  KebabCase = 'KebabCase',
+  PascalCase = 'PascalCase',
 }
