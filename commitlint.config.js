543fc7190a3030b91827f6c4e823fcfd0d6af04b
Mohamed Kamel <muhamed.kamel.elsayed@gmail.com>
2025-11-12 02:05:14 +0200

    chore: migrate ESLint configuration to new format and add Rollup build setup
    - Removed old ESLint configuration file (.eslintrc.js) and replaced it with a new ESLint configuration file (eslint.config.js) using the latest plugins and settings.
    - Introduced Rollup configuration (rollup.config.js) for building the project, supporting both ESM and CommonJS formats.
    - Updated package.json to reflect changes in build scripts and added necessary Rollup plugins.
    - Updated TypeScript configuration files (tsconfig.json and tsconfig.build.json) to use ESNext module resolution.
    - Added commitlint configuration (commitlint.config.js) for standardized commit message formatting.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   /*
    * Resolve and load @commitlint/config-conventional from node_modules.
    * Referenced packages must be installed
@@ -81,3 +81,6 @@ module.exports = {
     },
   },
 };
+
+
+
