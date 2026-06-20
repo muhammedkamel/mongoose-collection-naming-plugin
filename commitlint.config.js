92df392bd57afdcfa6979378bec1c6e02858f761
Mohamed Kamel <muhamed.kamel.elsayed@gmail.com>
2026-06-20 05:39:36 +0300

    chore!: update commitlint parser preset to conventional-changelog-conventionalcommits (#77)
    - Changed the parser preset in commitlint configuration from conventional-changelog-atom to conventional-changelog-conventionalcommits for improved commit message formatting.

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,11 +4,7 @@ export default {
    * Referenced packages must be installed
    */
   extends: ['@commitlint/config-conventional'],
-  /*
-   * Resolve and load conventional-changelog-atom from node_modules.
-   * Referenced packages must be installed
-   */
-  parserPreset: 'conventional-changelog-atom',
+  parserPreset: 'conventional-changelog-conventionalcommits',
   /*
    * Resolve and load @commitlint/format from node_modules.
    * Referenced package must be installed
